647551b2a811a82f6a5be008d790ccd343528001
ryu8114 <nogardwons@gmail.com>
2020-07-15 12:14:30 +0900

    initialcommit

---
+++ layouts/default.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="app-layout">
+    <div class="sidebar">
+      <p>チャンネル一覧</p>
+      <p>#general</p>
+      <p>#random</p>
+      <p>#random</p>
+    </div>
+    <div class="main-content">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+.app-layout {
+  display: flex;
+}
+.sidebar {
+  width: 300px;
+  background: #4A4141;
+  height: 100vh;
+  padding: 20px;
+}
+.sidebar p {
+  color: #ddd;
+  padding-top: 4px;
+}
+.main-content {
+  width: 100%;
+  background: #F1F1F1;
+  height: 100vh;
+}
+</style>
