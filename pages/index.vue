897287d71d143394ff7fc7348b87e228d1615bed
ryu8114 <nogardwons@gmail.com>
2020-08-01 21:09:42 +0900

    5-2. ElementUI でログインモーダルを作ろう

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container">
-    <button v-on:click="login">ログイン</button>
   </div>
 </template>
 
@@ -8,16 +7,6 @@
 import { firebase } from '~/plugins/firebase'
 export default {
   methods: {
-    login() {
-      const provider = new firebase.auth.GoogleAuthProvider()
-      firebase.auth().signInWithPopup(provider)
-        .then((result) => {
-          const user = result.user
-          console.log(user)
-        }).catch((error) => {
-          window.alert(error)
-        })
-    }
   }
 }
 </script>
